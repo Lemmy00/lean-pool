bf80767ec1e0b13ee6366bc9126735edc50fc955
Vasily Ilin <vasilin97@gmail.com>
2026-05-18 00:51:34 +0900

    Vendor Brauer group core

---
+++ LeanPool/BrauerGroupNew.lean
@@ -0,0 +1,28 @@
+/-
+Copyright (c) 2026 Yunzhou Xie and contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yunzhou Xie, Yichen Feng, Jujian Zhang, Yael Dillies
+-/
+
+import LeanPool.BrauerGroupNew.CentralSimple
+import LeanPool.BrauerGroupNew.Centralizer
+import LeanPool.BrauerGroupNew.ExtendScalar
+import LeanPool.BrauerGroupNew.FieldCat
+import LeanPool.BrauerGroupNew.LemmasAboutSimpleRing
+import LeanPool.BrauerGroupNew.MatrixCenterEquiv
+import LeanPool.BrauerGroupNew.MatrixEquivTensor
+import LeanPool.BrauerGroupNew.TwoSidedIdeal
+import LeanPool.BrauerGroupNew.Wedderburn
+import LeanPool.BrauerGroupNew.Mathlib
+import LeanPool.BrauerGroupNew.Subfield
+
+/-!
+# Brauer Group Core
+
+Source: url:https://github.com/Whysoserioushah/BrauerGroup_new
+Authors: Yunzhou Xie, Yichen Feng, Jujian Zhang, Yael Dillies
+Status: verified
+Main declarations: `Wedderburn_Artin`, `IsCentralSimple.TensorProduct.simple`, `CSA_implies_CSA`
+Tags: algebra, ring-theory, central-simple-algebras, brauer-groups
+MSC: 16K20, 16K50, 16S35
+-/
---
+++ LeanPool/BrauerGroupNew/Subfield.lean
@@ -0,0 +1,14 @@
+/-
+Copyright (c) 2026 Yunzhou Xie and contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yunzhou Xie, Yichen Feng, Jujian Zhang, Yael Dillies
+-/
+
+import LeanPool.BrauerGroupNew.Subfield.Defs
+import LeanPool.BrauerGroupNew.Subfield.FiniteDimensional
+
+/-!
+# Brauer Group New Subfield
+
+Import index for the Brauer group formalization.
+-/
