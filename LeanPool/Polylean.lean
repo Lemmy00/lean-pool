203dfb64efb5fad4fa32c9a85a3bfd801bae75da
Vasily Ilin <vasilin97@gmail.com>
2026-05-18 17:09:48 +0900

    Vendor Polylean unit conjecture infrastructure

---
+++ LeanPool/Polylean.lean
@@ -0,0 +1,18 @@
+/-
+Copyright (c) 2026 Siddhartha Gadgil, Anand Rao. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Siddhartha Gadgil, Anand Rao
+-/
+
+import LeanPool.Polylean.UnitConjecture
+
+/-!
+# Polylean Unit Conjecture Infrastructure
+
+Source: url:https://github.com/siddhartha-gadgil/Polylean
+Authors: Siddhartha Gadgil, Anand Rao
+Status: verified
+Main declarations: `LeanPool.Polylean.P.torsionFree`
+Tags: algebra, group-theory, ring-theory, unit-conjecture
+MSC: 16S34, 20F65
+-/
