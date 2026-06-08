c395775b55865070628a48bb87b2264e7bd7c15c
Vasily Ilin <vasilin97@gmail.com>
2026-06-07 19:52:38 -0700

    Add Brouwer source references

--- LeanPool/Brouwer.lean
+++ LeanPool/Brouwer.lean
@@ -10,7 +10,7 @@ import LeanPool.Brouwer.Primitive
 /-!
 # Existence of Nash equilibria via Brouwer's fixed-point theorem
 
-Source: url:https://github.com/math-xmum/Brouwer
+Source: doi:10.1073/pnas.36.1.48, url:https://github.com/math-xmum/Brouwer
 Authors: Math_XMUM
 Status: verified
 Main declarations: `ExistsNashEq`, `Brouwer`, `Brouwer_Product`, `IndexedLOrder.Scarf`, `FinGame`
